a6be62ed494864b23a883ed2cb559087c19cf6a9
Antonio Fernández Villa <fdez2002@gmail.com>
2023-01-11 09:41:30 +0100

    Creacio del menu

--- app/src/main/java/com/example/ejercicio_kotlin_men_opciones_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/ejercicio_kotlin_men_opciones_kotlin/MainActivity.kt
@@ -27,11 +27,13 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-
+        /*
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+         */
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -39,16 +41,24 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-
+/*
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         return when (item.itemId) {
+
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+
+
+
+
     }
+
+         */
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
